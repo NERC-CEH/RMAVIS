e7201e806a7fa27edb45ccee8bc9aca0cdd21428
ZekeMarshall <marshall_zeke@outlook.com>
2023-11-30 14:46:54 +0000

    Working on validating input data

--- modules/uploadData_server.R
+++ modules/uploadData_server.R
@@ -9,6 +9,9 @@ uploadData <- function(input, output, session) {
   # }) |>
   #   bindEvent(sidebar_options(), ignoreInit = TRUE)
   
+  columnNames_correct <- reactiveVal()
+  yearValues_numeric <- reactiveVal()
+  speciesNames_correct <- reactiveVal()
   
   observe({
     
@@ -18,12 +21,47 @@ uploadData <- function(input, output, session) {
       
       uploadDataTable <- DT::datatable(
         data = uploaded_data_raw,
-        editable = TRUE
+        editable = FALSE
       )
       
       return(uploadDataTable)
       
     })
+    
+    # First check that the column names are correct
+    
+    columnNames_correct <- all(colnames(uploaded_data_raw) %in% c("Year", "Site", "Quadrat.Group", "Quadrat", "Species", "Cover"))
+    columnNames_correct(columnNames_correct)
+    
+    output$columnNames_correct_expression <- shiny::renderText({
+      
+      paste0("Column Names Correct: ", as.character(columnNames_correct))
+      
+    })
+    
+    
+    if(columnNames_correct == TRUE){
+      
+      yearValues_numeric <- all(is.numeric(uploaded_data_raw$Year))
+      speciesNames_correct <- unique(uploaded_data_raw$Species) %in% speciesNames
+      
+      yearValues_numeric(yearValues_numeric)
+      speciesNames_correct(speciesNames_correct)
+      
+      output$yearValues_numeric_expression <- shiny::renderText({
+        
+        paste0("Year Values Numeric: ", as.character(yearValues_numeric))
+        
+      })
+      
+      output$speciesNames_correct_expression <- shiny::renderText({
+        
+        paste0("Species Names Correct: ", as.character(speciesNames_correct))
+        
+      })
+      
+    }
+    
     
   }) |>
     bindEvent(input$uploadDataInput,
--- modules/uploadData_ui.R
+++ modules/uploadData_ui.R
@@ -7,15 +7,49 @@ uploadDataUI <- function(id) {
     shiny::column(
       width = 12,
       
-      shiny::h5("Survey Data Table"),
-      
-      shiny::fileInput(inputId = ns("uploadDataInput"),
-                       label = "Browse",
-                       accept = c("text/csv",
-                                  "text/comma-separated-values,text/plain",
-                                  ".csv"
-                       )
+      bslib::layout_columns(
+        
+        col_widths = c(8, 4),
+        
+        shiny::fileInput(inputId = ns("uploadDataInput"),
+                         label = "Browse",
+                         accept = c("text/csv",
+                                    "text/comma-separated-values,text/plain",
+                                    ".csv"
+                         )
+        )
+        
       ),
+      
+      shiny::div(shiny::br()),
+      
+      shiny::h5("Uploaded Data"),
+      
+      shiny::markdown(
+        "
+        MAVIS expects data to be uploaded in .csv format with the following columns.
+        
+        - *Year:* Contains the year the quadrat was surveyed. Numeric.
+        - *Site:* Contains the name of the site containing the quadrat. String.
+        - *Quadrat.Group:* Contains the quadrat group. String.
+        - *Quadrat:* Contains the quadrat ID. String.
+        - *Species:* Contains the species, please download the list of accepted species names using the button above. String.
+        - *Cover:* Contains the species-quadrat cover estimation, a value between 0 and 1. Numeric.
+        
+        Formatting issues are displayed below.
+        
+        "
+      ),
+      
+      shiny::div(
+        
+        shiny::textOutput(outputId = ns("columnNames_correct_expression")),
+        shiny::textOutput(outputId = ns("yearValues_numeric_expression")),
+        shiny::textOutput(outputId = ns("speciesNames_correct_expression"))
+        
+      ),
+      
+      shiny::div(shiny::hr()),
       
       shiny::div(
         DT::DTOutput(outputId = ns("uploadDataTable"))
